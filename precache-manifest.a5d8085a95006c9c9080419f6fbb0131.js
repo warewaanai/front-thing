f72c48bae531ac43e51fc45926905d317509c13f
warewaanai <anai@DESKTOP-223FB3L.localdomain>
2020-04-13 12:15:53 +0300

    Updates

---
+++ precache-manifest.a5d8085a95006c9c9080419f6fbb0131.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fa30f12067a0860391028ad19c10ab",
+    "url": "/front-thing/index.html"
+  },
+  {
+    "revision": "cba379a0c371ddd605b2",
+    "url": "/front-thing/static/css/main.162c2b4e.chunk.css"
+  },
+  {
+    "revision": "84fffbe77b96f52a57f1",
+    "url": "/front-thing/static/js/2.f4fe98e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/front-thing/static/js/2.f4fe98e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba379a0c371ddd605b2",
+    "url": "/front-thing/static/js/main.a7407a63.chunk.js"
+  },
+  {
+    "revision": "5c5e01083aea84350d10",
+    "url": "/front-thing/static/js/runtime-main.f52a8a25.js"
+  },
+  {
+    "revision": "dfb158c51784a8f1b87ab6c6b25e6a78",
+    "url": "/front-thing/static/media/2.dfb158c5.jpg"
+  },
+  {
+    "revision": "976c1df949b3e61ef46fef3690e105f3",
+    "url": "/front-thing/static/media/Palanquin-Bold.976c1df9.ttf"
+  },
+  {
+    "revision": "28ae298ab5f035df8508e6ff1af7d0e5",
+    "url": "/front-thing/static/media/Palanquin-Regular.28ae298a.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/front-thing/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "17b573894ed72c1303e934d6bf56cf2f",
+    "url": "/front-thing/static/media/facebook.17b57389.svg"
+  },
+  {
+    "revision": "ff629c02385f2f3a199b7e0b3065d077",
+    "url": "/front-thing/static/media/google.ff629c02.png"
+  },
+  {
+    "revision": "2e969c144e1c8a451e8c8114679b9d66",
+    "url": "/front-thing/static/media/inv_flogo.2e969c14.png"
+  },
+  {
+    "revision": "14228c7a6cea369958dfb2708b4624dc",
+    "url": "/front-thing/static/media/twitter.14228c7a.png"
+  }
+]);
